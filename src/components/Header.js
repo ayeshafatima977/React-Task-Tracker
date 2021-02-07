25378d6a4b7f310d2ceada9d2b084287cc28201a
Ayesha Fatima <ayeshafatima977@gmail.com>
2021-02-06 19:29:59 -0700

    CSS Style for heading in JS;

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import PropTypes from "prop-types";
 const Header = ({ title }) => {
   return (
     <header>
-      <h1>{title}</h1>
+      <h1 style={headingStyle}>{title}</h1>
     </header>
   );
 };
-
+//Default Title in case no title is provided
 Header.defaultProps = {
   title: "Task Tracker",
 };
@@ -15,4 +15,9 @@ Header.propTypes = {
   title: PropTypes.string.isRequired,
 };
 
+//CSS in JS
+const headingStyle = {
+  color: "red",
+  backgroundColor: "black",
+};
 export default Header;
