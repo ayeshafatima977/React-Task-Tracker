d34ef4a84beb7ed6e79711a0f9e585de7ec29de0
Ayesha Fatima <ayeshafatima977@gmail.com>
2021-02-07 18:56:50 -0700

    Refactored Task component by globally defining the state by using useState Hook;

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { useState } from "react";
+const App = () => {
+  //Setting default value of functions as an array of tasks
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "Learn React", day: "Feb 7 at 5:00pm", reminder: true },
+    { id: 2, text: "Learn Rest API", day: "Feb 8 at 5:00pm", reminder: false },
+    {
+      id: 3,
+      text: "Personal Development Session",
+      day: "Feb 8 at 7:00pm",
+      reminder: true,
+    },
+  ]);
 
-function App() {
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
-}
+};
 
 export default App;
